4f2cc221e1110c42fdb247efd933eea74a7d948b
Jobin Johnson <jobinrjohnson@gmail.com>
2019-11-30 23:13:03 +0530

    View: Added response parsing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,29 +11,65 @@
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+
+    <style>
+        .main_container {
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+        .card{
+            width: 80%;
+            border: none;
+        }
+        .form-control,.btn{
+            border-radius: 0;
+        }
+        #res_card{
+            display: none;
+        }
+        a{
+            color: inherit;
+        }
+    </style>
+
 </head>
 
 <body>
 
 
-    <div class="container">
+    <div class="container main_container">
 
-        <div class="card">
-            <div class="card-body">
-
-
-                <form method="post" action="api/shorten">
-                    <div class="form-group" id="link_container">
-                        <input type="url" class="form-control" name="link" id="linkControl" required
-                            placeholder="Link to Shorten, eg: https://..">
-                        <div class="invalid-feedback" id="invalid_feedback">
+            <div class="card" id="maincard">
+                <div class="card-body ">
+                    <h1>URL Shortner.</h1>
+    
+                    <br>
+                    <form method="post" action="api/shorten">
+                        <div class="row">
+    
+                            <div class="form-group col pr-0 mr-0" id="link_container">
+                                <input type="url" class="form-control" name="link" id="linkControl" required
+                                    placeholder="Link to Shorten, eg: https://..">
+                                <div class="invalid-feedback" id="invalid_feedback">
+                                </div>
+                            </div>
+                            <div class="col-auto pl-0 mr-0">
+                                <input type="submit" class="btn btn-primary" value="Shorten">
+                            </div>
                         </div>
-                    </div>
-                    <input type="submit" class="btn btn-primary">
-                </form>
-
+                    </form>
+                    <br><br><br>
+                </div>
             </div>
-        </div>
+
+            <div class="card" id="res_card">
+
+                <p>Hey, Here is the shortned URL</p>
+                <h1><a href="" class="shorned_url"></a></h1>
+            </div>
 
     </div>
 
@@ -49,7 +85,19 @@
         crossorigin="anonymous"></script>
 
     <script>
+
+
+        function IgotTheURL(url) {
+            $("#maincard").slideUp()
+            $("#res_card").slideDown()
+            $("#res_card .shorned_url").attr("href", url);
+            $("#res_card .shorned_url").text(url);
+        }
+
         $("form").on("submit", function (e) {
+            IgotTheURL("{{url('/')}}/" + "xyz")
+
+
             e.preventDefault();
             $.post(
                 'api/shorten',
